960f7f6f6f4d9921b737b979e136a4ba84912097
chan <kohdc1723@gmail.com>
2022-09-02 16:56:37 -0700

    refactor using module

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
-
-const template = {
-    html: function(title, list, control, body) {
-        return `
-            <!doctype html>
-            <html>
-            <head>
-                <title>WEB2 - Node.js - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">Node.js</a></h1>
-                ${list}
-                ${control}
-                ${body}
-            </body>
-            </html>
-        `;
-    },
-    list: function(fileList) {
-        let list = "<ul>";
-        let i = 0;
-        while (i < fileList.length) {
-            list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-            ++i;
-        }
-        list += "</ul>";
-
-        return list;
-    }
-}
+const template = require("./lib/template");
 
 const app = http.createServer((req, res) => {
     let _url = req.url;
