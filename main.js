2ed8daf1dc2d2d439c4ce8abd13351df9db9a2ca
chan <kohdc1723@gmail.com>
2022-09-02 15:39:15 -0700

    update function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, control, body) {
     return `
         <!doctype html>
         <html>
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
         <body>
             <h1><a href="/">Node.js</a></h1>
             ${list}
+            ${control}
             ${body}
         </body>
         </html>
@@ -40,14 +41,17 @@ const app = http.createServer((req, res) => {
         // welcome page
         fs.readdir("./data", (err, fileList) => {
             let title = "Welcome";
-            let description = "Hello, Node.js!"
+            let description = "Hello, Node.js!";
             let body = `
                 <h2>${title}</h2>
                 <article>${description}</article>
             `;
             let list = templateList(fileList);
+            let control = `
+                <a href="/create">Create</a>
+            `;
 
-            let template = templateHTML(title, list, body);
+            let template = templateHTML(title, list, control, body);
 
             res.writeHead(200);
             res.end(template);
@@ -62,11 +66,106 @@ const app = http.createServer((req, res) => {
                     <article>${description}</article>
                 `;
                 let list = templateList(fileList);
-                
-                let template = templateHTML(title, list, body);
+                let control = `
+                    <a href="/update?id=${title}">Update</a>
+                `;
+
+                let template = templateHTML(title, list, control, body);
     
                 res.writeHead(200);
                 res.end(template);
+            });
+        });
+    } else if (pathName === "/create") {
+        // create page
+        fs.readdir("./data", (err, fileList) => {
+            let title = "Create";
+            let body = `
+                <h2>${title}</h2>
+                <article>
+                    <form action="/create_process" method="post">
+                        <p><input type="text" name="title" placeholder="title"></input></p>
+                        <p><textarea name="description" placeholder="description"></textarea></p>
+                        <p><input type="submit" value="create"></input></p>
+                    </form>
+                </article>
+            `;
+            let list = templateList(fileList);
+            let control = "";
+
+            let template = templateHTML(title, list, control, body);
+
+            res.writeHead(200);
+            res.end(template);
+        });
+    } else if (pathName === "/create_process") {
+        // create process
+        let body = "";
+
+        req.on("data", (data) => {
+            body += data;
+        });
+
+        req.on("end", () => {
+            let post = new URLSearchParams(body);
+            let title = post.get("title");
+            let description = post.get("description");
+            
+            fs.writeFile(`./data/${title}`, description, "utf8", (err) => {
+                res.writeHead(302, {
+                    Location: `/?id=${title}`
+                });
+                res.end();
+            });
+        });
+    } else if (pathName === "/update") {
+        // update page
+        fs.readdir("./data", (err, fileList) => {
+            fs.readFile(`./data/${queryData.id}`, "utf8", (err, description) => {
+                let title = queryData.id;
+                let body = `
+                    <h2>${title}</h2>
+                    <article>
+                        <form action="/update_process" method="post">
+                            <input type="hidden" name="old-title" value="${title}"></input>
+                            <p><input type="text" name="new-title" value="${title}"></input></p>
+                            <p><textarea name="description" placeholder="description">${description}</textarea></p>
+                            <p><input type="submit" value="update"></input></p>
+                        </form>
+                    </article>
+                `;
+                let list = templateList(fileList);
+                let control = `
+                    <a href="/update?id=${title}">Update</a>
+                `;
+
+                let template = templateHTML(title, list, control, body);
+    
+                res.writeHead(200);
+                res.end(template);
+            });
+        });
+    } else if (pathName === "/update_process") {
+        // update process
+        let body = "";
+
+        req.on("data", (data) => {
+            body += data;
+        });
+        
+        req.on("end", () => {
+            let post = new URLSearchParams(body);
+            let oldTitle = post.get("old-title");
+            let newTitle = post.get("new-title");
+            let description = post.get("description");
+            
+            fs.rename(`./data/${oldTitle}`, `./data/${newTitle}`, (err) => {
+                fs.writeFile(`./data/${newTitle}`, description, "utf8", (err) => {
+                    res.writeHead(302, {
+                        Location: `/?id=${newTitle}`
+                    });
+                    res.end();
+                });
             });
         });
     } else {
